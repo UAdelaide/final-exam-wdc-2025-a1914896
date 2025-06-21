76538cb06b53168a112d6341ac422840f4659478
a1914896 <quan.liu@student.adelaide.edu.au>
2025-06-21 05:39:38 +0000

    [automatic commit] 2025-06-21 05:39:38 UTC 175.45.84.198

--- part1/app.js
+++ part1/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const dogsRouter = require('./routes/dogs');
-const db = require('./db'); // 假设你已经有 db.js 连接池文件
+const walkRequestsRouter = require('./routes/walkrequests');
+const db = require('./db');
 
 app.use(express.json());
-app.use('/api/dogs', dogsRouter);
 
-// 启动服务器
+app.use('/api/dogs', dogsRouter);
+app.use('/api/walkrequests', walkRequestsRouter); // ← 新增的路由注册
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
