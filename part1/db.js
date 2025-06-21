ecb54d37244c5d987af03b4fee2a09e0fce6d0a1
a1914896 <quan.liu@student.adelaide.edu.au>
2025-06-21 06:01:12 +0000

    [automatic commit] 2025-06-21 06:01:12 UTC ;; communications error to 208.67.222.222#53: timed out 175.45.84.198

--- part1/db.js
+++ part1/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',           // ← 如果你设置了密码，写进去
+  password: '',
   database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
