7b4764d54d25ad82afa326222bd7be853724d0c5
a1914896 <quan.liu@student.adelaide.edu.au>
2025-06-21 07:10:51 +0000

    [automatic commit] 2025-06-21 07:10:51 UTC ;; communications error to 208.67.222.222#53: timed out 175.45.84.198

--- part2/app.js
+++ part2/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
-require('dotenv').config();
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
-
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/walks', walkRoutes);
+const app = express();
+const PORT = 8080;
+
+// 添加中间件
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 设置 session
+app.use(session({
+  secret: 'mySecretKey',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// 路由
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
-module.exports = app;
+// 默认页面
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
