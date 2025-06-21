71915edb09036f19da7cd0cc435f68f364b21e04
a1914896 <quan.liu@student.adelaide.edu.au>
2025-06-21 05:43:08 +0000

    [automatic commit] 2025-06-21 05:43:08 UTC 175.45.84.198

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
+// GET /api/walkrequests/open
 router.get('/open', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT
-        wr.id AS request_id,
-        d.name AS dog_name,
-        wr.requested_time,
-        wr.duration_minutes,
-        wr.location,
-        u.username AS owner_username
+      SELECT wr.id AS request_id, d.name AS dog_name, wr.requested_time,
+             wr.duration_minutes, wr.location, u.username AS owner_username
       FROM walk_requests wr
       JOIN dogs d ON wr.dog_id = d.id
       JOIN users u ON d.owner_id = u.id
@@ -25,3 +21,4 @@ router.get('/open', async (req, res) => {
 });
 
 module.exports = router;
+
